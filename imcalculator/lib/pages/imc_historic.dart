e92c7dc384f4a4f0f2eb0c4759b466178b8f17cb
Vinicius Recoaro <vrecoaro@hotmail.com>
2023-06-23 18:59:59 -0300

    Fixed card list, now the informations are been exhibited

--- imcalculator/lib/pages/imc_historic.dart
+++ imcalculator/lib/pages/imc_historic.dart
@@ -27,62 +27,57 @@ class _ImcHistoricPageState extends State<ImcHistoricPage> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      appBar: AppBar(title: const Text("IMC Historic")),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          dateController.text = "";
-          heightController.text = "";
-          weightController.text = "";
-          showDialog(
-              context: context,
-              builder: (BuildContext bc) {
-                return AlertDialog(
-                  title: const Text("IMC Calculator"),
-                  content: Wrap(
-                    children: [
-                      const Text("Data"),
-                      TextField(controller: dateController),
-                      const Padding(
-                        padding: EdgeInsets.only(top: 16),
-                        child: Text("Altura"),
-                      ),
-                      TextField(controller: heightController),
-                      const Padding(
-                        padding: EdgeInsets.only(top: 16),
-                        child: Text("Peso"),
-                      ),
-                      TextField(controller: weightController),
-                    ],
-                  ),
-                  actions: [
-                    TextButton(
-                        onPressed: () {
-                          imcRepository.addImc(IMC(
-                              dateController.text,
-                              double.parse(heightController.text),
-                              double.parse(weightController.text)));
-                          Navigator.pop(context);
-                          setState(() {});
-                        },
-                        child: const Text("Salvar"))
-                  ],
-                );
-              });
-        },
-        backgroundColor: const Color.fromARGB(255, 26, 64, 95),
-        child: const Icon(Icons.add),
-      ),
-      // body: Container(
-      //   child: Expanded(
-      //     child: ListView.builder(
-      //         itemCount: _imcList.length,
-      //         itemBuilder: (BuildContext bc, int index) {
-      //           return CardLabel(
-      //               date: _imcList[index].date,
-      //               imc: _imcList[index].returnIMC());
-      //         }),
-      //   ),
-      // )
-    ));
+            appBar: AppBar(title: const Text("IMC Historic")),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                dateController.text = "";
+                heightController.text = "";
+                weightController.text = "";
+                showDialog(
+                    context: context,
+                    builder: (BuildContext bc) {
+                      return AlertDialog(
+                        title: const Text("IMC Calculator"),
+                        content: Wrap(
+                          children: [
+                            const Text("Data"),
+                            TextField(controller: dateController),
+                            const Padding(
+                              padding: EdgeInsets.only(top: 16),
+                              child: Text("Altura"),
+                            ),
+                            TextField(controller: heightController),
+                            const Padding(
+                              padding: EdgeInsets.only(top: 16),
+                              child: Text("Peso"),
+                            ),
+                            TextField(controller: weightController),
+                          ],
+                        ),
+                        actions: [
+                          TextButton(
+                              onPressed: () {
+                                imcRepository.addImc(IMC(
+                                    dateController.text,
+                                    double.parse(heightController.text),
+                                    double.parse(weightController.text)));
+                                Navigator.pop(context);
+                                setState(() {});
+                              },
+                              child: const Text("Salvar"))
+                        ],
+                      );
+                    });
+              },
+              backgroundColor: const Color.fromARGB(255, 26, 64, 95),
+              child: const Icon(Icons.add),
+            ),
+            body: ListView.builder(
+                itemCount: _imcList.length,
+                itemBuilder: (BuildContext bc, int index) {
+                  return CardLabel(
+                      date: _imcList[index].date,
+                      imc: _imcList[index].returnIMC());
+                })));
   }
 }
